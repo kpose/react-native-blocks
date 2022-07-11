2da69c36f8059c9b983989a8a2530cf104f84d0a
!Kpose <ganihujude@gmail.com>
2022-07-11 10:12:54 +0100

    ???

--- template/index.js
+++ template/index.js
@@ -6,6 +6,8 @@
  * @format
  */
 import './shim';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
