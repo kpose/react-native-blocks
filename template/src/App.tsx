7e3916c2c5dadf221d02a0595a28b46119c17691
!Kpose <ganihujude@gmail.com>
2022-07-28 18:27:37 +0100

    provision web3

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import RootNavigator from './navigation/RootNavigator';
+import Web3 from 'web3';
 
 const App = () => {
+  const web3 = new Web3('http://localhost:7545');
+  const newWallet = web3.eth.accounts.wallet.create(1);
+  const newAccount = newWallet[0];
+  console.log(newAccount);
   return <RootNavigator />;
 };
 
