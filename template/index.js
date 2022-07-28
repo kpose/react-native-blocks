6a2abdb9dbf791d8dccb32a1f8230f8ce13f843b
!Kpose <ganihujude@gmail.com>
2022-07-28 19:17:54 +0100

    provision ethers

--- template/index.js
+++ template/index.js
@@ -4,6 +4,8 @@
 
 import 'react-native-gesture-handler';
 import './shim';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
