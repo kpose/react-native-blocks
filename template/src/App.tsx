b7ceb9d1338187eb429534c5edb04cc86eb2ac12
!Kpose <ganihujude@gmail.com>
2022-07-29 11:44:11 +0100

    remove redundant code

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import RootNavigator from './navigation/RootNavigator';
-import Web3 from 'web3';
 import {Platform} from 'react-native';
 import WalletConnectProvider from '@walletconnect/react-native-dapp';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
-  const web3 = new Web3('http://localhost:7545');
-  const newWallet = web3.eth.accounts.wallet.create(1);
-  const newAccount = newWallet[0];
-
   const deepLinklUriScheme = 'yourdeeplinkuri://';
-
-  console.log(newAccount);
   return (
     <WalletConnectProvider
       redirectUrl={
